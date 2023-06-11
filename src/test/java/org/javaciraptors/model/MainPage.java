886eeb566d0e47cc81b5525d87a44d618785883c
BayAzat <azatbay.job@gmail.com>
2023-06-11 23:24:44 +0500

    1st commit

---
+++ src/test/java/org/javaciraptors/model/MainPage.java
@@ -0,0 +1,10 @@
+package org.javaciraptors.model;
+
+import org.openqa.selenium.WebDriver;
+
+public class MainPage extends BasePage {
+
+    public MainPage(WebDriver driver) {
+        super(driver);
+    }
+}

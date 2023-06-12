27e174ca626ecbe958ab2c1c855fff7222de9c3d
Artem <artem.savichev@mail.ru>
2023-06-12 16:14:05 +0600

    add second test

--- src/test/java/org/javaciraptors/AutomationexerciseTest.java
+++ src/test/java/org/javaciraptors/AutomationexerciseTest.java
@@ -22,4 +22,15 @@ public class AutomationexerciseTest extends BaseTest {
         buttonClose.click();
     }
 
+    @Test
+    public void secondTest() {
+        final String price = "1590.00";
+
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[class='picture'] " +
+                "img[title='Show details for 14.1-inch Laptop']"))).click();
+        String laptopPrice = getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//div[@class='product-price']"))).getText().trim();
+
+        Assert.assertEquals(laptopPrice, price);
+    }
 }

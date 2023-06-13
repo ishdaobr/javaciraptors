984a0b9704ab07441794cd451d2099d1035031b6
BayAzat <azatbay.job@gmail.com>
2023-06-13 11:37:28 +0500

    commit

--- src/test/java/org/javaciraptors/model/MainPage.java
+++ src/test/java/org/javaciraptors/model/MainPage.java
@@ -1,10 +1,16 @@
 package org.javaciraptors.model;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class MainPage extends BasePage {
 
     public MainPage(WebDriver driver) {
         super(driver);
+    }
+
+    public MainPage clickRegister() {
+        getDriver().findElement(By.xpath("//a[normalize-space()='Register']")).click();
+        return this;
     }
 }

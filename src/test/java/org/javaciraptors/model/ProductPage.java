9c6a3d6dc4f47ec6c5546868aaa6c3b9bcfc3162
Artem <artem.savichev@mail.ru>
2023-06-13 16:52:38 +0600

    добавил ожидание отображения к методам страницы продукта

--- src/test/java/org/javaciraptors/model/ProductPage.java
+++ src/test/java/org/javaciraptors/model/ProductPage.java
@@ -2,6 +2,7 @@ package org.javaciraptors.model;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ProductPage extends BasePage {
 
@@ -10,35 +11,43 @@ public class ProductPage extends BasePage {
     }
 
     public String getPrice() {
-        return getDriver().findElement(By.xpath("//div[@class='product-price']")).getText().trim();
+        return getWait2().until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//div[@class='product-price']"))).getText().trim();
     }
 
     public String getName() {
-        return getDriver().findElement(By.xpath("//h1[@itemprop='name']")).getText().trim();
+        return getWait2().until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//h1[@itemprop='name']"))).getText().trim();
     }
 
     public String getShortDescription() {
-        return getDriver().findElement(By.xpath("//div[@class='short-description']")).getText().trim();
+        return getWait2().until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//div[@class='short-description']"))).getText().trim();
     }
 
     public String getFullDescription() {
-        return getDriver().findElement(By.xpath("//div[@class='full-description']")).getText().trim();
+        return getWait2().until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//div[@class='full-description']"))).getText().trim();
     }
 
     public ProductPage addToCart() {
-        getDriver().findElement(By.xpath("//input[@class='button-1 add-to-cart-button']")).click();
+        getWait2().until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//input[@class='button-1 add-to-cart-button']"))).click();
         return this;
     }
 
     public Double getRating() {
-        return Double.parseDouble(getDriver().findElement(By.xpath("//span[@itemprop='ratingValue']")).getText().trim());
+        return Double.parseDouble(getWait2().until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//span[@itemprop='ratingValue']"))).getText().trim());
     }
 
     public Integer getReviewsCount() {
-        return Integer.parseInt(getDriver().findElement(By.xpath("//span[@itemprop='reviewCount']")).getText().trim());
+        return Integer.parseInt(getWait2().until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//span[@itemprop='reviewCount']"))).getText().trim());
+
     }
 
     public String getStockStatus() {
-        return getDriver().findElement(By.xpath("//div[@class='stock']/span[@class='value']")).getText().trim();
+        return getWait2().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='stock']/span[@class='value']"))).getText().trim();
     }
 }

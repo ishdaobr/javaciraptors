209e98d86d96ea7f56ede7b6a99d912cff6fffe4
Artem <artem.savichev@mail.ru>
2023-06-13 19:10:55 +0600

    рефакторинг созданных тестов продукта

--- src/test/java/org/javaciraptors/ProductTest.java
+++ src/test/java/org/javaciraptors/ProductTest.java
@@ -3,7 +3,6 @@ package org.javaciraptors;
 import org.javaciraptors.model.ProductPage;
 import org.javaciraptors.runner.BaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,8 +11,9 @@ public class ProductTest extends BaseTest {
     public void testCheckLaptopPriceTest() {
         final String price = "1590.00";
 
-        getWait5().until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//div[@class='picture']//img[@title='Show details for 14.1-inch Laptop']"))).click();
+        getDriver().findElement(
+                By.xpath("//div[@class='picture']//img[@title='Show details for 14.1-inch Laptop']")).click();
+
         String productPrice = new ProductPage(getDriver()).getPrice();
 
         Assert.assertEquals(productPrice, price);
@@ -23,8 +23,8 @@ public class ProductTest extends BaseTest {
     public void testCheckProductName() {
         final String name = "14.1-inch Laptop";
 
-        getWait10().until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//div[@class='picture']//img[@title='Show details for 14.1-inch Laptop']"))).click();
+        getDriver().findElement(
+                By.xpath("//div[@class='picture']//img[@title='Show details for 14.1-inch Laptop']")).click();
 
         String productName = new ProductPage(getDriver()).getName();
 
@@ -35,11 +35,12 @@ public class ProductTest extends BaseTest {
     public void testCheckBackToMainPage() {
         final String welcomeText = "Welcome to our store";
 
-        getWait2().until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//div[@class='picture']//img[@title='Show details for 14.1-inch Laptop']"))).click();
+        getDriver().findElement(
+                By.xpath("//div[@class='picture']//img[@title='Show details for 14.1-inch Laptop']")).click();
 
         new ProductPage(getDriver()).clickLogo();
-        String text = getDriver().findElement(By.xpath("//h2[@class='topic-html-content-header']")).getText().trim();
+        String text = getDriver().findElement(By.xpath("//h2[@class='topic-html-content-header']"))
+                .getText().trim();
 
         Assert.assertEquals(text, welcomeText);
     }
